2ffde122c4b2fe97f256d9f2a82c81cc4dc57dfc
Stanley Kariuki <stanleykariuki1999@gmail.com>
2025-01-16 15:59:25 +0300

    Deleted the backend server and started on the contracts for the backend and network. More like reinventing the wheel.

--- contracts/Ticket.sol
+++ contracts/Ticket.sol
@@ -4,6 +4,22 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Ticket is ERC721 {
+    address public owner;
+    uint256 public totalOccassions;
+    uint256 public totalSupply;
+
+    struct Occassion {
+        uint256 id;
+        string name;
+        uint256 cost;
+        uint256 tickets;
+        uint256 maxTickets;
+        string date;
+        string time;
+        string location;
+    }
+
+
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
     }
    
